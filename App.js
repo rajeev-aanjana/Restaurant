3fba988c772db38b7221a4afcf257c91529d3ba7
Rajeev Aanjana <rajeevanjana143@gmail.com>
2023-02-23 15:33:10 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Resturant from './component/basics/Resturant';
+const App = () => {
+  return (
+    <>
+      <Resturant />
+    </>
+  )
+};
+
+export default App;
